4de40f2a70de4ebcbd37fb2d904bbfd412bee76d
ClaytonMain <main.clayton@gmail.com>
2025-04-09 08:50:49 -0600

    Ignore error

--- src/scenes/introduction_to_raymarching/ExperimentShaderMaterial.tsx
+++ src/scenes/introduction_to_raymarching/ExperimentShaderMaterial.tsx
@@ -7,6 +7,7 @@ export const ExperimentShaderMaterial = shaderMaterial(
   {
     uTime: 0,
     uCameraPosition: new THREE.Vector3(0, 0, 0),
+    // @ts-expect-error doesn't like Euler
     uCameraRotation: new THREE.Euler(0, 0, 0),
     uResolution: new THREE.Vector2(100, 100),
   },
