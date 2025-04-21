470ddba1f7dc7774eef00794acdf9266dc208ae7
Tycho-Euler <main.clayton@gmail.com>
2025-04-21 12:31:29 -0600

    Slime mold start

---
+++ src/scenes/slime_mold/SlimeMoldScene.tsx
@@ -0,0 +1,42 @@
+import { Stats } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect } from "react";
+import Footer from "../../components/Footer";
+import SlimeMold from "./SlimeMold";
+
+export default function SlimeMoldScene() {
+  const backgroundColor = "#18042b";
+
+  useEffect(() => {
+    document.title = "Slime Mold Scene";
+    document.body.style.background = backgroundColor;
+  }, []);
+
+  return (
+    <>
+      <Canvas
+        gl={{
+          preserveDrawingBuffer: true,
+          toneMappingExposure: 1.5,
+        }}
+        className="touch-none"
+        dpr={Math.min(window.devicePixelRatio, 2)}
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [0, 0, 8],
+        }}
+        style={{ background: backgroundColor }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight color={"#fff"} intensity={0.5} />
+          <SlimeMold />
+          <Stats />
+        </Suspense>
+      </Canvas>
+      <Footer />
+    </>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
 import restart from "vite-plugin-restart";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react(),
     restart({
-      // restart: ["src/**/shaders/**/*", "src/**/*.tsx"],
-      restart: ["src/**/shaders/**/*"],
+      restart: ["src/**/shaders/**/*", "src/**/*.tsx"],
+      // restart: ["src/**/shaders/**/*"],
     }),
     glsl({
       watch: true,
@@ -19,5 +21,6 @@ export default defineConfig({
   ],
   server: {
     host: true,
+    open: true,
   },
 });
