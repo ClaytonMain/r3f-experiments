2447089ceae23678af0bb9dd752cf44548f7f417
ClaytonMain <main.clayton@gmail.com>
2025-04-23 13:36:45 -0600

    Update declaration file

--- src/scenes/slime_mold/types.d.ts
+++ src/scenes/slime_mold/types.d.ts
@@ -2,12 +2,14 @@ import { ReactThreeFiber } from "@react-three/fiber";
 import { SlimeMoldShaderMaterial } from "./SlimeMoldShaderMaterial";
 
 declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      slimeMoldShaderMaterial: ReactThreeFiber.Noed<
-        typeof SlimeMoldShaderMaterial &
-          JSX.IntrinsicElements["dreiShaderMaterial"]
-      >;
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        slimeMoldShaderMaterial: ReactThreeFiber.Node<
+          typeof SlimeMoldShaderMaterial &
+            JSX.IntrinsicElements["dreiShaderMaterial"]
+        >;
+      }
     }
   }
 }
